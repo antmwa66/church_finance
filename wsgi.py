85dc41f88eaca0783b852e467682c8214bf5c299
anmwat <anmwat@gmail.com>
2026-08-21 17:19:26 +0300

    Add environment variables directly to WSGI file for PythonAnywhere

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
 import sys
 import os
 
+# Set environment variables directly in WSGI file
+os.environ['SECRET_KEY'] = 'c9192975ed9d231ca1605ded0afb6041b4d06a34044b0b771c2d9ab4d2a24ceb'
+os.environ['FLASK_DEBUG'] = 'false'
+
 # Add the current directory to the Python path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
