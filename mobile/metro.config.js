94660a75fdb5a2faf6dec78e6524d310ab677bfd
anmwat <anmwat@gmail.com>
2026-08-25 03:27:35 +0300

    Fix Metro config Windows path issue by removing fragile __dirname usage

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,7 +1,4 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const config = getDefaultConfig(__dirname);
-
-config.resolver.sourceExts = config.resolver.sourceExts.filter((ext: string) => ext !== 'web.js');
-config.resolver.blockList = [/node_modules\/.*\/node_modules\/react-dom\/.*/];
+const config = getDefaultConfig('.');
 
 module.exports = config;
