572bed1f9b4aa181d4f69588085b21233c0fe04c
anmwat <anmwat@gmail.com>
2026-08-25 03:23:42 +0300

    Add DOMRect polyfill and metro blockList to fix Expo Go runtime error

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,6 +3,24 @@ import { ErrorBoundary } from './src/components/ErrorBoundary';
 import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
+if (typeof global.domRect === 'undefined') {
+  (global as any).domRect = class DOMRect {
+    x = 0;
+    y = 0;
+    width = 0;
+    height = 0;
+    top = 0;
+    left = 0;
+    bottom = 0;
+    right = 0;
+    constructor(x = 0, y = 0, width = 0, height = 0) {
+      this.x = x; this.y = y; this.width = width; this.height = height;
+      this.top = y; this.left = x; this.bottom = y + height; this.right = x + width;
+    }
+    toJSON() { return {}; }
+  };
+}
+
 export default function App() {
   return (
     <ErrorBoundary>
--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,3 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts = config.resolver.sourceExts.filter((ext: string) => ext !== 'web.js');
+config.resolver.blockList = [/node_modules\/.*\/node_modules\/react-dom\/.*/];
+
 module.exports = config;
